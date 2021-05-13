2b74c5ac4062b908c11681b53cdf3b0613b487e1
Brijesh Patadiya <51873202+brijeshsoni1300@users.noreply.github.com>
2021-05-13 09:11:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ import CityInfo from "./component/CityInfo";
 import LocationInfo from "./component/LocationInfo";
 import CityWeather from "./component/CityWeather";
 import LocationWeather from "./component/LocationWeather";
-
+/// this is comment from birju's side
 export { CurrentLocation, CityInfo, LocationInfo, CityWeather, LocationWeather};
